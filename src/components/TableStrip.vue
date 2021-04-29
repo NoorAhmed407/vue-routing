aaf4262bcec92116384c54db92c394bf10fa395e
Noor Ahmed <nq73399@gmail.com>
2021-04-29 18:37:46 +0500

    API CALL in Vue JS

---
+++ src/components/TableStrip.vue
@@ -0,0 +1,28 @@
+<template>  
+ 
+    
+      <th scope="row">{{user.id}}</th>
+      <td>{{user.login}}</td>
+      <td>
+        <img :src="user.avatar_url" alt="avatar" width="200" height="200">
+      </td>
+    
+</template>
+
+<script>
+
+export default {
+  name: 'TableStrip',
+  props: ['user'],
+  components: {},
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,56 @@
 <template>  
-  <h1>Home</h1>
-  </template>
+  <h1 class="text-center my-5">Home</h1>
+  <div class="container text-center">
+      <div class="spinner-border" v-if="loading === true" role="status">
+          <span class="sr-only">Loading...</span>
+      </div>
+    <table class="table table-striped text-center" v-else>
+  <thead>
+    <tr>
+      <th scope="col">S.No</th>
+      <th scope="col">Full Name</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+        <tr v-for="(user, index) in users" :key=index>
+          <TableStrip :user="user" />
+        </tr>
+      
+  </tbody>
+</table>
+  </div>
+</template>
 
 <script>
+  import TableStrip from './../components/TableStrip';
+  import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {}
+  data(){
+    return{
+      loading: true,
+      users: []
+    }
+  },
+  components: {TableStrip},
+  mounted(){
+   console.log('Mounted');
+   axios.get('https://api.github.com/users').then(res=> {
+     console.log('Data', res.data);
+     this.users = res.data;
+     this.loading = false;
+   }).catch(err=> {
+     this.loading = false;
+     console.log('Error', err);
+   })
+  }
 }
 </script>
 
-<style>
+<style scoped>
+
+
 
 </style>
